ea67cf5dc248948a874254f72f919ceb54e2b09a
amalwayscharan <charanforms@gmail.com>
2015-04-05 13:52:21 -0500

    setup

---
+++ Main.Designer.cs
@@ -0,0 +1,117 @@
+﻿namespace AverageDoseValues
+{
+    partial class Main
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.groupBox1 = new System.Windows.Forms.GroupBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.label1 = new System.Windows.Forms.Label();
+            this.btnBrowse = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
+            this.groupBox1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // groupBox1
+            // 
+            this.groupBox1.Controls.Add(this.button1);
+            this.groupBox1.Controls.Add(this.label1);
+            this.groupBox1.Controls.Add(this.btnBrowse);
+            this.groupBox1.Controls.Add(this.textBox1);
+            this.groupBox1.Location = new System.Drawing.Point(12, 12);
+            this.groupBox1.Name = "groupBox1";
+            this.groupBox1.Size = new System.Drawing.Size(440, 112);
+            this.groupBox1.TabIndex = 5;
+            this.groupBox1.TabStop = false;
+            this.groupBox1.Text = "Browse File and Read Data";
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(181, 81);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Read File";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(34, 40);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(29, 13);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "Path";
+            // 
+            // btnBrowse
+            // 
+            this.btnBrowse.Location = new System.Drawing.Point(342, 30);
+            this.btnBrowse.Name = "btnBrowse";
+            this.btnBrowse.Size = new System.Drawing.Size(75, 23);
+            this.btnBrowse.TabIndex = 1;
+            this.btnBrowse.Text = "Browse";
+            this.btnBrowse.UseVisualStyleBackColor = true;
+            this.btnBrowse.Click += new System.EventHandler(this.btnBrowse_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(101, 32);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(201, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // openFileDialog1
+            // 
+            this.openFileDialog1.FileName = "openFileDialog1";
+            // 
+            // Main
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(469, 136);
+            this.Controls.Add(this.groupBox1);
+            this.Name = "Main";
+            this.Text = "Average Doses";
+            this.groupBox1.ResumeLayout(false);
+            this.groupBox1.PerformLayout();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.GroupBox groupBox1;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button btnBrowse;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.OpenFileDialog openFileDialog1;
+    }
+}
+
---
+++ Main.cs
@@ -0,0 +1,195 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using Excel = Microsoft.Office.Interop.Excel;
+
+namespace AverageDoseValues
+{
+    public partial class Main : Form
+    {
+        public Main()
+        {
+            InitializeComponent();
+        }
+        String filePath = "";
+        public static int curMonth = 0;
+        public static int curYear = 0;
+        public static bool initDateFlag = true;
+        List<decimal> listOfAvgDoseValues = new List<decimal>();
+        List<string> listOfdateValues = new List<string>();
+        private static Excel.Workbook MyBook = null;
+        private static Excel.Application MyApp = null;
+        private static Excel.Worksheet MySheet = null;
+
+
+        private void releaseObject(object obj)
+        {
+            try
+            {
+                System.Runtime.InteropServices.Marshal.ReleaseComObject(obj);
+                obj = null;
+            }
+            catch (Exception ex)
+            {
+                obj = null;
+                MessageBox.Show("Unable to release the Object " + ex.ToString());
+            }
+        }
+
+        private void btnBrowse_Click(object sender, EventArgs e)
+        {
+            
+            if (openFileDialog1.ShowDialog() == DialogResult.OK)
+            {
+                System.IO.StreamReader sr = new
+                   System.IO.StreamReader(openFileDialog1.FileName);
+            }
+            filePath = openFileDialog1.FileName;
+            textBox1.Text = filePath; // getting the file name from user input.
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+             Excel.Range range;
+            MyApp = new Excel.Application();
+            MyApp.Visible = false;
+            MyBook = MyApp.Workbooks.Open(filePath); //Giving the path to excel workbook to open and read the file.
+            MySheet = (Excel.Worksheet)MyBook.Sheets[1]; // Explicit cast is not required here
+
+            string str = "";
+            int rCnt, cCnt, dateCol = 0, doseCol = 0;
+            range = MySheet.UsedRange; //range contains all the cells taht are currently in use.
+            decimal avgOfDose = 0;
+            int count = 0;
+            //loop to iterate through the columns which contain Total and  Date, which we use to find the average values!
+            //Cursor points to the required fields.
+            for (cCnt = 1; cCnt <= range.Columns.Count; cCnt++)
+            {
+                str = Convert.ToString((range.Cells[1, cCnt] as Excel.Range).Value2);
+                if (!String.IsNullOrEmpty(str))
+                {
+                    if (str.Contains("Date")) // Storing the column number in dateCol which has Date in the first row
+                    {
+                        dateCol = cCnt;
+
+                    }
+                    if (str.Contains("Total")) // Storing the column number in doseCol which has Total in the first row.
+                    {
+
+                        doseCol = cCnt;
+                    }
+                }
+            }
+            //loop to iterate through the rows that are taken from the first loop, this takes all the date values and total dose values in the selected file. 
+            for (rCnt = 2; rCnt <= range.Rows.Count; rCnt++)
+            {
+
+                DateTime dateOfTEPC = new DateTime();
+                decimal doseValue = 0;
+                bool isValidTEPCDate = false; // Boolean to check for valid TEPC date
+                bool isValidDoseValue = false; // Boolean to check for valid Dose Value
+
+
+                object inpDate = (range.Cells[rCnt, dateCol] as Excel.Range).Value2; // gets the range of cells, rCnt is the rown count which contains the current row number of the particular column number of date
+                if (inpDate != null)
+                {
+                    if (inpDate is double) // check if date is double
+                    {
+                        dateOfTEPC = DateTime.FromOADate((double)inpDate); // conver from OADate to Date data type
+                        isValidTEPCDate = true;
+                    }
+                    else
+                    {
+                        DateTime.TryParse((string)inpDate, out dateOfTEPC); //  Parsing the input date to DateTime
+                        isValidTEPCDate = true;
+                    }
+
+
+                }
+
+
+                object dValue = (range.Cells[rCnt, doseCol] as Excel.Range).Value2; // Gets the range of cells which contain dose values, rCnt is the variable which iterated through all the dose values  of the aprticular row.
+                if (dValue != null) //check for dose value for null. the cell in the excel may be empty
+                {
+                    if (dValue is double)
+                    {
+                        doseValue = Convert.ToDecimal(dValue);
+                        isValidDoseValue = true;
+                    }
+                }
+
+                if (isValidDoseValue && isValidTEPCDate) // Check if both Dose and TEPCDate are valid
+                {
+
+                    if (initDateFlag) // Initial date flag to check for the cursor reaches another month row.
+                    {
+                        curMonth = dateOfTEPC.Month;
+                        curYear = dateOfTEPC.Year;
+                        listOfdateValues.Add(dateOfTEPC.ToOADate().ToString()); // Adding corresponding date to listOdateValues list.
+                        initDateFlag = false; // Update the boolean 
+                    }
+
+
+                    if (dateOfTEPC.Month == curMonth && dateOfTEPC.Year == curYear) // Incrementing the count (used to divide sum of dose values with) 
+                    //and adding the dose values by checking current month and year
+                    {
+                        count++;
+                        avgOfDose += doseValue;
+
+                    }
+                    else
+                    {
+                        curMonth = dateOfTEPC.Month;
+                        curYear = dateOfTEPC.Year;
+                        avgOfDose = avgOfDose / count; // calculating the average dose value
+                        listOfdateValues.Add(dateOfTEPC.ToOADate().ToString());
+                        // listOfYearValues.Add(curYear);
+                        listOfAvgDoseValues.Add(avgOfDose);
+                        count = 1; //Updating the count value for further iterations of for loop
+                        //  MessageBox.Show(avgOfDose.ToString());
+                        avgOfDose = doseValue;
+                    }
+                }
+
+            }
+            listOfAvgDoseValues.Add(avgOfDose / count); // Finally adding the average dose values to the list
+
+
+
+            MessageBox.Show("Read Successfull");
+            try
+            {
+                MyBook.Close(true, filePath, null);
+                MyApp.Quit();
+                releaseObject(MySheet);
+                releaseObject(MyBook);
+                releaseObject(MyApp);
+            }
+            catch (Exception ex)
+            {
+                MessageBox.Show("Unable to close spreadsheet");
+            }
+
+
+            System.IO.StreamWriter sw = new System.IO.StreamWriter(System.IO.Directory.GetCurrentDirectory()+"/AverageDoses.csv");
+
+            for (int ii = 0; ii < listOfAvgDoseValues.Count; ii++)
+            {
+
+                sw.WriteLine(listOfdateValues[ii] + " ,  " + listOfAvgDoseValues[ii]); // Writing the obtained Avg Dose values into a CSV file with respect to the corresponding dates.
+
+            }
+
+            sw.Close();
+            MessageBox.Show("Write Successful");
+
+        
+        }
+    }
+}
